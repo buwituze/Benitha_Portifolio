6bb250a0e4c00a2f5c5cf29c0817f6ba55d027b8
buwituze <b.rutagengw@alustudent.com>
2024-08-28 16:23:13 +0200

    final designs

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function showmore() {
+    var addprojects = document.getElementById("furtherprojects");
+    if (addprojects.style.display === "none") {
+        addprojects.style.display = "block";
+        
+    } else {
+        addprojects.style.display = "none";
+
+    }
+}
