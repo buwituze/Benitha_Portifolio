b9bd668515ab6fe2bfe989ee4f872bc40e5d192c
buwituze <b.rutagengw@alustudent.com>
2024-09-08 09:59:48 +0200

    projects

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 function showmore() {
     var addprojects = document.getElementById("furtherprojects");
-    if (addprojects.style.display === "none") {
+    var toggleButton = document.getElementById("toggleButton");
+
+    if (addprojects.style.display === "none" || addprojects.style.display === "") {
         addprojects.style.display = "block";
-        
+        toggleButton.textContent = "Show Less";
     } else {
         addprojects.style.display = "none";
-
+        toggleButton.textContent = "Show More";
     }
 }
