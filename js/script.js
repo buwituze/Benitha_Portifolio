54436eec8ec795cf0259ba9655731053a89335d0
buwituze <b.rutagengw@alustudent.com>
2024-09-09 23:44:07 +0200

    integrating emailing system EmailJS

--- js/script.js
+++ js/script.js
@@ -8,6 +8,27 @@
 //     emailjs.send("service_6mu3m8w", "template_02sekg1", params).then(alert("Your message has been sent!"));
 // };
 
+// function sendMail(event) {
+//     event.preventDefault(); // Prevent the form from submitting traditionally
+
+//     let params = {
+//         name: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         subject: document.getElementById("subject").value,
+//         message: document.getElementById("message").value
+//     };
+
+//     emailjs.send("service_6mu3m8w", "template_02sekg1", params)
+//         .then((response) => {
+//             alert("Your message has been sent successfully!");
+//             console.log('Success!', response.status, response.text);
+//         })
+//         .catch((error) => {
+//             alert("Failed to send the message. Please try again.");
+//             console.error('Error:', error);
+//         });
+// }
+
 function sendMail(event) {
     event.preventDefault(); // Prevent the form from submitting traditionally
 
@@ -21,6 +42,8 @@ function sendMail(event) {
     emailjs.send("service_6mu3m8w", "template_02sekg1", params)
         .then((response) => {
             alert("Your message has been sent successfully!");
+            document.forms["contact"].reset(); // Reset the form
+            location.reload(); // Reload the page to ensure the form is empty
             console.log('Success!', response.status, response.text);
         })
         .catch((error) => {
