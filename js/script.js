f5ba1d51cebafee0bf42624de5a64b5e84d30332
buwituze <b.rutagengw@alustudent.com>
2024-09-10 00:12:37 +0200

    adjustments

--- js/script.js
+++ js/script.js
@@ -1,36 +1,5 @@
-// function sendMail() {
-//     let params = {
-//         name: document.getElementById("name").value,
-//         email: document.getElementById("email").value,
-//         subject: document.getElementById("subject").value,
-//         message: document.getElementById("message").value
-//     };
-//     emailjs.send("service_6mu3m8w", "template_02sekg1", params).then(alert("Your message has been sent!"));
-// };
-
-// function sendMail(event) {
-//     event.preventDefault(); // Prevent the form from submitting traditionally
-
-//     let params = {
-//         name: document.getElementById("name").value,
-//         email: document.getElementById("email").value,
-//         subject: document.getElementById("subject").value,
-//         message: document.getElementById("message").value
-//     };
-
-//     emailjs.send("service_6mu3m8w", "template_02sekg1", params)
-//         .then((response) => {
-//             alert("Your message has been sent successfully!");
-//             console.log('Success!', response.status, response.text);
-//         })
-//         .catch((error) => {
-//             alert("Failed to send the message. Please try again.");
-//             console.error('Error:', error);
-//         });
-// }
-
 function sendMail(event) {
-    event.preventDefault(); // Prevent the form from submitting traditionally
+    event.preventDefault(); 
 
     let params = {
         name: document.getElementById("name").value,
@@ -42,8 +11,8 @@ function sendMail(event) {
     emailjs.send("service_6mu3m8w", "template_02sekg1", params)
         .then((response) => {
             alert("Your message has been sent successfully!");
-            document.forms["contact"].reset(); // Reset the form
-            location.reload(); // Reload the page to ensure the form is empty
+            document.forms["contact"].reset(); 
+            location.reload();
             console.log('Success!', response.status, response.text);
         })
         .catch((error) => {
